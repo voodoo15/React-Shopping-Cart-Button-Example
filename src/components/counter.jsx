bcbb5f46f3331ef12999ce5970e155b8fcd7bfaa
Arnold Redoblado <VOODOO15@VOODOO15-MBP.lan>
2018-10-13 17:44:34 -0400

    Updated counter.jsx to render label that will display a count and button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <React.Fragment>
+        <span className='badge badge-warning m-2'>{this.formatCount()}</span>
+        <button className='btn btn-secondary btn-sm'>increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( < Counter / > , document.getElementById('root'));
 
 serviceWorker.unregister();
