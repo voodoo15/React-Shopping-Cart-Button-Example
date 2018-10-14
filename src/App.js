181691e456993121ea35fad6d464443eac2dd8fb
Arnold Redoblado <VOODOO15@VOODOO15-MBP.lan>
2018-10-14 09:22:16 -0400

    Added counter to navbar if counter components have an increment of 1 or more

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar totalCounter={this.state.counters.filter(c => c.value > 0).length} />
         <main className="container">
           <Counters
             counters={this.state.counters}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,11 +5,15 @@ class NavBar extends Component {
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          Navbar
+          Navbar {" "}
+          <span className="badge badge-pill badge-secondary">
+          {this.props.totalCounter}
+          </span>
         </a>
       </nav>
     );
   };
+
 };
 
 export default NavBar;
