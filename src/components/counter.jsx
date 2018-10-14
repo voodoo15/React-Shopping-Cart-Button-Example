be48c1917c3a8b281a766766f5fc88708b51dcce
Arnold Redoblado <VOODOO15@VOODOO15-MBP.lan>
2018-10-14 07:00:28 -0400

    Added counters.jsx to handle list of counters.  Updated index.js and counter.jsx to accomodate new counters component.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,12 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value
   };
 
   handleIncrement = () => {
-    this.setState({count: this.state.count + 1});
-  }
+    this.setState({value: this.state.value + 1});
+  };
 
   render() {
 
@@ -17,21 +17,22 @@ class Counter extends Component {
         <button
           className="btn btn-secondary btn-sm"
           onClick={this.handleIncrement}
-          >increment</button>
+        >increment
+        </button>
       </React.Fragment>
     );
-  }
+  };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
-  }
+  };
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-}
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  };
+};
 
 export default Counter;
