25e775be69528d0d08d0db1e4ea141166832f23b
Arnold Redoblado <VOODOO15@VOODOO15-MBP.lan>
2018-10-14 08:01:09 -0400

    Creating navbar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-// import Counter from './components/counter';
 import Counters from './components/counters';
 
 
